8adb8aea9ae6130e884f34e580e206166e60bf43
Wen <64304185+Wind-Storm-X@users.noreply.github.com>
2025-02-26 00:20:56 -0700

    Add files via upload
    Task 5: Vector100DKey implementation

---
+++ src/lshfindex/Vector100DKey.java
@@ -0,0 +1,35 @@
+package LSHFIndex;
+
+import global.Vector100Dtype;
+
+public class Vector100DKey {
+
+  private Vector100Dtype key;
+
+  public String toString(){
+     return key.toString();
+  }
+
+  /** Class constructor
+   *  @param     value   the value of the Vector 100D key to be set 
+   */
+  public Vector100DKey(Vector100Dtype value) 
+  { 
+    key=new Vector100Dtype(value.getDimension());
+  }
+
+  /** get a copy of the vector 100D key
+   *  @return the reference of the copy 
+   */
+  public Vector100Dtype getKey() 
+  {
+    return new Vector100Dtype(key.getDimension());
+  }
+
+  /** set the integer key value
+   */  
+  public void setKey(Vector100Dtype value) 
+  { 
+    key=new Vector100Dtype(value.getDimension());
+  }
+}
